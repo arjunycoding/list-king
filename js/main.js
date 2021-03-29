57f701df75a89f6210e245f691bc011d9f18e8cd
arjunyuvaraj <arjunycoding@gmail.com>
2021-03-29 12:57:34 -0400

    adding an animation to delete an list item (li)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let item = $("#itemDescription");
 let add = $("#addBtn");
 let newItemButton = $("#newItemButton");
 let validate = $(".validate");
+let li = $("ul");
 validate.hide();
 item.hide();
 add.hide();
@@ -26,5 +27,26 @@ function addItem(){
     add.hide();
     newItemButton.show();
 }
+function deleteItem(el){
+    $(this).animate({
+        opacity: 0.0,
+        paddingLeft: '+=80'
+      }, 500, function() {
+        $(this).remove();
+      });
+}
+
+function doit(){
+    console.log("hii");    
+}
+li.on('click','li', function() {
+    $(this).animate({
+        opacity: 0.0,
+        paddingLeft: '+=80'
+      }, 500, function() {
+        $(this).remove();
+      })
+})
+
 newItemButton.on('click', requestItem);
 add.on('click', addItem);
