a58e4eaa18271e5933f9712c19af52ec284fa51a
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-05-22 12:10:00 -0400

    making login.js more readable

--- js/login.js
+++ js/login.js
@@ -7,8 +7,10 @@ let passwordJS = document.getElementById("pwd");
 let savedUname = localStorage.getItem("uname");
 let savedPwd = localStorage.getItem("pwd");
 error.hide();
+// auto filling usernaem's and passowrds
 username.val(savedUname);
 password.val(savedPwd);
+// hiding and showing passowerd funcion
 function togglePassword() {
     if (passwordJS.type == "password") {
         passwordJS.type = "text";
@@ -16,6 +18,7 @@ function togglePassword() {
         passwordJS.type = "password";
     }
 }
+// form validation functions
 function validateForm(){
     if(checkLogin(username.val(), password.val())) {
         error.hide();
@@ -36,7 +39,7 @@ function checkLogin(username, password) {
     };
     return array[username] == encrypt(password);
 }
-
+// encrypting
 function encrypt(password) {
     return CryptoJS.MD5(password).toString();
 }
@@ -44,71 +47,7 @@ function storeLogin(){
     localStorage.setItem("uname", username.val());
     localStorage.setItem("pwd", password.val());
 }
-function autoSubmit(){
-    if(username.length == 0){
-        return
-    } else {
-        validateForm()
-    }
-}
-autoSubmit()
+// adding event listeners
 showpwd.on('click', togglePassword);
 loginbtn.on('click', validateForm);
 password.on('change', validateForm);
-/*
-let submit = $("#submit")
-let error = $(".error")
-error.hide()
-let info = {
-    "vidya" : "123",
-    "yuvaraj" : "456",
-    "adhithya" : "readMe123",
-    "arjun" : "101112"
-}
-function storeLgin(){
-    let uname = $("#uname").val()
-    let pwd = $("#pwd").val()
-    localStorage.setItem('username', uname);
-    localStorage.setItem('passowrd', pwd);
-}
-function autoFillLogin(){
-    let uname = $("#uname")
-    let pwd = $("#pwd")
-    uname.val(localStorage.getItem('username'))
-    pwd.val(localStorage.getItem('passowrd'))
-}
-autoFillLogin()
-function validateForm(){
-    let uname = $("#uname").val()
-    let pwd = $("#pwd").val()
-    console.log(`Username :: ${uname}`)
-    console.log(`Password :: ${pwd}`)
-    for (const [key, value] of Object.entries(info)) {
-        console.log(key + value)
-        if(key == uname && value == pwd){
-            storeLgin()
-            console.log('Sucsees')
-            window.location = "webBook.html"
-            break
-        } else {
-            error.show()
-    }
-    }
-}
-function autoSubmit(){
-    let uname = $("#uname").val()
-let pwd = $("#pwd").val()
-    if(uname == localStorage.getItem('username') && pwd == localStorage.getItem('passowrd')){
-        validateForm()
-    } else {
-        return
-    }
-}
-autoSubmit()
-// let uname = $("#uname").val()
-// let pwd = $("#pwd").val()
-// // console.log(uname == localStorage.getItem('username') && pwd == localStorage.getItem('passowrd'))
-// console.log(`LOCAL SOTRAGE :: username == ${localStorage.getItem('username')}|| password == ${localStorage.getItem('passowrd')}`)
-// console.log(uname + pwd )
-submit.on("click", validateForm)
-*/ 
